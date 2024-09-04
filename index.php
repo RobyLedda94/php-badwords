09ba7e31e7c65f4118e853400b5566d5a05ac118
RobyLedda94 <robyledda01@yahoo.it>
2024-09-04 16:23:59 +0200

    [UPD] creata la struttura html con form e bottone, in serito nell'attributo action il file in cui voglio trasmettere i dati

--- index.php
+++ index.php
@@ -3,14 +3,34 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>PHP BADWORDS</title>
 </head>
 <body>
-
-    
-    <?php 
-        $nome = "Roberto"
-    ?>
-    <h1><?php echo $nome; ?></h1>
+    <div class="container mt-4 text-center">
+        <div class="row">
+            <div class="col-12">
+                <form action="./form.php" method="GET">
+                    <div class="row">
+                        <div class="col-12 col-sm-4">
+                            <div class="form-group">
+                                <div class="control-label"><strong>Inserisci qui il paragrafo:</strong></div>
+                                <textarea name="paragrafo" id="paragrafo" placeholder="Inserire paragrafo" class="form-control"></textarea>
+                            </div>
+                        </div>
+                        <div class="col-12 col-sm-4">
+                            <div class="form-group">
+                                <div class="control-label"><strong>Inserisci qui la parola da censurare:</strong></div>
+                                <input type="text" name="parola" id="parola" placeholder="Inserire parola" class="form-control">
+                            </div>
+                        </div>
+                            <div class="col-12 mt-4 d-flex justify-content-start">
+                                <button type="submit" class="btn btn-primary">Invia</button>
+                            </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
