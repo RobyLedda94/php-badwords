91f3ccc32a6e81a8d9841946e5d76349a55679aa
RobyLedda94 <robyledda01@yahoo.it>
2024-09-04 17:39:46 +0200

    [UPD] ok struttura html nel file form.php

--- form.php
+++ form.php
@@ -9,14 +9,21 @@ $parola = $_GET['parola'];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
-    <?php 
-        echo "
-        <div class="container"></div>
-        
-        "
-    ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="content text-center">
+                    <h1>Il tuo paragrafo :</h1>
+                    <p><?php echo $paragrafo ?></p>
+                    <h2>La tua parola :</h2>
+                    <p><?php echo $parola ?></p>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
