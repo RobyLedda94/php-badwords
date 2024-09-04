79372d830a79c0a366cc25773627d6104e572a27
RobyLedda94 <robyledda01@yahoo.it>
2024-09-04 17:05:47 +0200

    [UPD] recupero e mostro a video i valori inseriti nel campo input

--- form.php
+++ form.php
@@ -1,1 +1,18 @@
-Debug di prova per raggiungere il file from.php
+<?php
+// recupero i dati inseriti nel campo input 
+$paragrafo = $_GET['paragrafo'];
+$parola = $_GET['parola'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- mostro a video i valori inseriti nei campi input -->
+    <?php echo $paragrafo." ".$parola; ?>
+</body>
+</html>
